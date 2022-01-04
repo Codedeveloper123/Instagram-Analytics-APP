ccbea65d6c28e49ff70aa7f2c1099adb2db377f8
Codedeveloper123 <jtj4@illinois.edu>
2022-01-04 00:25:11 -0600

    removed sensitive API Access

--- instagram-analytics-script/defines.py
+++ instagram-analytics-script/defines.py
@@ -6,11 +6,9 @@ from requests.models import Response
 
 def getCreds():
     creds = dict()
-    creds[
-        "access_token"
-    ] = "EAALq4Ha3VOQBAOOcLZAmisCubEebyokkyuKBbuhngqZB54iTFd4YWYcKtxEOdJrc7bM0DhJjTqbkMmKYx4mi9QxquGrfFQPlFJHqG5cLlqpwbRuAJfIscHeoZAMZAZAzbykAg69o1qaIrAJvSpKGZCZA9ZAwxitoBvqprZAwIR6BRba6n2AJMRuZBTvacfNqpULXOCnEOzZBXSRdTF9GNoGacJe"
-    creds["client_id"] = "821199738721508"
-    creds["client_secret"] = "a109b5ed1861bb3fc544ab1e6d0409d2"
+    creds["access_token"] = "ACESS-Token #"
+    creds["client_id"] = "FaceBook AP ID"
+    creds["client_secret"] = "Facebook Ap Secret ID"
     creds["graph_domain"] = "https://graph.facebook.com/"
     creds["graph_version"] = "v12.0"
     creds["endpoint_base"] = creds["graph_domain"] + creds["graph_version"] + "/"
