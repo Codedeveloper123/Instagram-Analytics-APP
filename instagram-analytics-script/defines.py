8b1672f22f524dcd2a2e7ea376f8b961781c22a1
Codedeveloper123 <jtj4@illinois.edu>
2022-01-04 19:00:45 -0600

    Added scripts to get fb page access token, id, and instagram account id

--- instagram-analytics-script/defines.py
+++ instagram-analytics-script/defines.py
@@ -6,13 +6,20 @@ from requests.models import Response
 
 def getCreds():
     creds = dict()
-    creds["access_token"] = "ACESS-Token #"
-    creds["client_id"] = "FaceBook AP ID"
-    creds["client_secret"] = "Facebook Ap Secret ID"
+    creds[
+        "access_token"
+    ] = "EAALq4Ha3VOQBANKPBLDXoC09TEZBsqwkUZCjbbYT1n3B69Vbv6YTKDzM9RZBdqqIdC88CKBBLYMPCs06V5OJAtdlgzYZC8gBZBQQpjxeeVWpRHjKE1MBqa6ovxVaIELyS2hsQIeeMnXnuJzYz6jD3EAOQN1QZC3ayq9cRUZCR83Rw1uySmQ2o8JqS6e2q8nWhhgWRbXVUcxVxuwJ1PzdJyb"
+    creds["client_id"] = "821199738721508"
+    creds["client_secret"] = "a109b5ed1861bb3fc544ab1e6d0409d2"
     creds["graph_domain"] = "https://graph.facebook.com/"
     creds["graph_version"] = "v12.0"
     creds["endpoint_base"] = creds["graph_domain"] + creds["graph_version"] + "/"
     creds["debug"] = "none"
+    creds["page_id"] = "104070632162114"
+    creds[
+        "page_access_token"
+    ] = "EAALq4Ha3VOQBAN8u9G5TUFbQvAqbjVZB7Awf6EHtNR9HxpBpCbLm0pwHx0tZCq3RnEayQvesm4CyR66lz160iIRQi5rU2c58ochqiUVYIhwporwFzuETaZARIxg2zYZC3zTctFaMxSWVflvviuLEmXFrfVf7Tki4lxdI4oGF0pWi6wrHyNoZAZB7qNeHQecZAaUMN6xWZA7ZBXWhDojvwqdKq"
+    creds["instagram_account"] = "937109979701395"
     return creds
 
 
@@ -21,9 +28,9 @@ def makeApiCall(url, endpointParams, debug="none"):
     response = dict()
     response["url"] = url
     response["endpoint_params"] = endpointParams
-    response["endpoint_params_pretty"] = json.dump(endpointParams, indent=4)
+    response["endpoint_params_pretty"] = json.dumps(endpointParams, indent=4)
     response["json_data"] = json.loads(data.content)
-    response["kson_data_pretty"] = json.dump(response["json_data"], indent=4)
+    response["json_data_pretty"] = json.dumps(response["json_data"], indent=4)
     if "yes" == debug:
         displayApiCallData(response)
     return response
